26f9a2bdbdd3fabc49dfb4eb6946acd3e927668f
KaMa75 <karolmarszalek@o2.pl>
2019-03-19 11:20:34 +0100

    Konfiguracja PROXY

--- front/src/Components/App.jsx
+++ front/src/Components/App.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+// import axios from 'axios';
 
 class App extends Component {
+    componentDidMount() {
+        fetch('/api/users/auth')
+            .then(response => {
+                console.log(response)
+            });
+        // axios.get('/api/users/auth')
+        //     .then(response => {
+        //         console.log(response)
+        //     });
+    }
     render() {
         return (
             <div>
