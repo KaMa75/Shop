b68c1fc19a923ff117a8405ca57c520dfd0db90f
KaMa75 <karolmarszalek@o2.pl>
2019-03-21 14:00:47 +0100

    header

--- front/src/Components/Header/Header.jsx
+++ front/src/Components/Header/Header.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from './Logo.jsx';
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <header>
+                <div className="container">
+                    <Logo />
+                    <nav className="clear-fix">
+                        <div className="user-nav">
+                            <ul className="clear-fix">
+                                <li>                                    
+                                    <Link to="#" replace>
+                                        Zarejestruj
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="#" replace>
+                                        Zaloguj
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="#" replace>
+                                        Koszyk
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="main-nav">
+                            <ul className="clear-fix">
+                                <li>
+                                    <Link to="#" replace>
+                                        Damskie
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="#" replace>
+                                        Męskie
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="#" replace>
+                                        Dziecięce
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </header>
         );
     }
 }
---
+++ front/src/Components/Header/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <Link to="/" replace>
+                <img src="../../../images/logo.png" alt=""/>
+            </Link>
+        </div>
+    );
+}
+
+export default Logo;
