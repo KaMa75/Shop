4abd6bf2160ff931a4df0fde04c14e3d8085b91b
KaMa75 <karolmarszalek@o2.pl>
2019-03-21 09:56:23 +0100

    Dodanie nagłówka i stopki

--- front/src/Components/MainPage/MainPage.jsx
+++ front/src/Components/MainPage/MainPage.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header.jsx';
+import Footer from '../Footer/Footer.jsx';
 
 class MainPage extends Component {
     render() {
         return (
             <div>
+                <Header />
                 MainPage
+                <Footer />
             </div>
         );
     }
