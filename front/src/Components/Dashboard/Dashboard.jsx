f6c33c5a9a75257559516e52bd89bcb81509f820
KaMa75 <karolmarszalek@o2.pl>
2019-03-27 15:26:31 +0100

    style w dashboard

--- front/src/Components/Dashboard/Dashboard.jsx
+++ front/src/Components/Dashboard/Dashboard.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import LayoutDashboard from '../LayoutDashboard.jsx';
+import Button from '../Button.jsx';
 
 class Dashboard extends Component {
     render() {
         return (
             <LayoutDashboard>
-                dashboard
+                <div className="user-nfo-panel-wrapper">
+                    <h3>Informacje o użytkowniku</h3>
+                    <div className="user-nfo-panel">
+                        <p>name</p>
+                        <p>lastname</p>
+                        <p>email</p>
+                        <Button
+                            linkTo='/user/user_profile'
+                        >
+                            Edytuj dane
+                        </Button>
+                    </div>
+                </div>
+                <div className="user-nfo-panel-wrapper">
+                    <h3>Historia zakupów</h3>
+                    <div className="user-nfo-panel">
+                        <span>historia</span>
+                    </div>
+                </div>
             </LayoutDashboard>
         );
     }
