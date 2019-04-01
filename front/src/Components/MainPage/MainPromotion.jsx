8b2ed711cdcfd2de949b44039bcf5ad58f5fd0a8
KaMa75 <karolmarszalek@o2.pl>
2019-04-01 14:12:28 +0200

    dodanie warunku sprawdzającego istnienie danych do banera promocyjnego

--- front/src/Components/MainPage/MainPromotion.jsx
+++ front/src/Components/MainPage/MainPromotion.jsx
@@ -7,27 +7,30 @@ console.log(promotion.btnText)
 
 class MainPromotion extends Component {
     render() {
-        return (
-            <div className="home-promotion">
-                <div className="promotion-img"
-                    style={{
-                        background: `url(${promotion.img})`
-                    }}
-                >
-                    <div className="container">
-                        <div className="promotion-content">
-                            <h1>{ promotion.header }</h1>
-                            <p>{ promotion.content }</p>
-                            <Button
-                                linkTo={ promotion.linkTo }
-                            >
-                                { promotion.btnText }
-                            </Button>
+        if(promotion) {
+            return (
+                <div className="home-promotion">
+                    <div className="promotion-img"
+                        style={{
+                            background: `url(${promotion.img})`
+                        }}
+                    >
+                        <div className="container">
+                            <div className="promotion-content">
+                                <h1>{ promotion.header }</h1>
+                                <p>{ promotion.content }</p>
+                                <Button
+                                    linkTo={ promotion.linkTo }
+                                >
+                                    { promotion.btnText }
+                                </Button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
+        return null;
     }
 }
 
