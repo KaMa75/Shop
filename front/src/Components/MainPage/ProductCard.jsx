3ee83c59c67c67a50d7f32784670dde5e0c87910
KaMa75 <karolmarszalek@o2.pl>
2019-04-01 20:25:00 +0200

    ADD akcje do przycisków w karcie produktu

--- front/src/Components/MainPage/ProductCard.jsx
+++ front/src/Components/MainPage/ProductCard.jsx
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '../Button.jsx';
 
-const genCardImage = (images) => {
-    if(images.length > 0) {
-        return images[0].url;
-    } else {
-        return '/images/img_not_available.jpg';
+class ProductCard extends Component {
+
+    genCardImage = (images) => {
+        if(images.length > 0) {
+            return images[0].url;
+        } else {
+            return '/images/img_not_available.jpg';
+        }
+    };
+
+    addToBasket = (id) => () => {
+        console.log(`Dodaj do koszyka: ${id}`);
     }
-};
-
-const ProductCard = (props) => {
-    console.log(props);
-    return (
-        <div className='product-card-container'>
-            <div className="product-image">
-                <img src={ genCardImage(props.images) } alt=""/>
+    
+    render() {
+        return (
+            <div className='product-card-container'>
+                <div className="product-image">
+                    <img src={ this.genCardImage(this.props.images) } alt=""/>
+                </div>
+                <div className="product-card-content">
+                    <h2>{ this.props.manufacturer.name }</h2>
+                    <h4>{ this.props.name }</h4>
+                    <h2>{ `${this.props.price} zł` }</h2>
+                </div>
+                <div className="product-card-buttons">
+                    <Button
+                        linkTo={`/product_details/${this.props._id}`}
+                    >
+                        Zobacz produkt
+                    </Button>
+                    <button
+                        onClick={ this.addToBasket(this.props._id) }
+                    >
+                        Do koszyka
+                    </button>
+                </div>
             </div>
-            <div className="product-card-content">
-                <h2>{ props.manufacturer.name }</h2>
-                <h4>{ props.name }</h4>
-                <h2>{ `${props.price} zł` }</h2>
-            </div>
-            <div className="product-card-buttons">
-                <button>
-                    Zobacz produkt
-                </button>
-                <button>
-                    Kup
-                </button>
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default ProductCard;
