1d7ba4658e75c4f1ae58ebf52ceb0f4c68ee0fb6
KaMa75 <karolmarszalek@o2.pl>
2019-04-01 14:39:01 +0200

    pobieranie najczęściej sprzedawanych produktów

--- front/src/Components/MainPage/MainPage.jsx
+++ front/src/Components/MainPage/MainPage.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import MainSlider from './MainSlider.jsx';
 import MainPromotion from './MainPromotion.jsx';
 
+const urlArticles = '/api/product/articles';
+
 class MainPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            productsBySell: [],
+            productsByArrival: []
+        };
+    }
+
+    getProductsBySell() {
+        axios.get(`${urlArticles}?sortBy=sold&order=desc&limit=4`)
+        .then(response => response.data)
+        .then(response => {
+            console.log(response);
+        })
+    }
+
+
+
     render() {
         return (
             <div className='main-page'>
@@ -14,6 +36,7 @@ class MainPage extends Component {
             </div>
         );
     }
+
 }
 
 export default MainPage;
