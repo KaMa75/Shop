63d633d91481552f3c688455c6aefec8874d2866
KaMa75 <karolmarszalek@o2.pl>
2019-04-01 21:07:26 +0200

    INSTALL Material UI, ADD Loader

--- front/src/Components/Loading.jsx
+++ front/src/Components/Loading.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const Loading = () => {
     return (
-        <div>
-            <h1>Ładoanie</h1>
+        <div className='loader'>
+            <CircularProgress
+                style={{
+                    color: '#2b252c'
+                }}
+                size={ 40 }
+            />
         </div>
     );
 };
