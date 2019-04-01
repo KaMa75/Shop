0d3418238cbe62032f7d98040fd9643d51f96b95
KaMa75 <karolmarszalek@o2.pl>
2019-04-01 13:52:56 +0200

    zmiana ścieżki do folderu data

--- front/src/Components/LayoutDashboard.jsx
+++ front/src/Components/LayoutDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation.jsx';
-import { dashUserNav } from '../data/dashNavData';
+import { dashUserNav } from '../configData/dashNavData';
 
 class LayoutDashboard extends Component {
     render() {
